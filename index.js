f4ebec8876de11b91af4d1871a9d13769f4f6b07
Weizhen Sheng <weizhensheng@nichols-ve501-0991.apn.wlan.upenn.edu>
2018-05-06 23:09:34 -0400

    update index.html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname, '/', 'piglatin.html'));
+	response.sendFile(path.join(__dirname, '/', 'index.html'));
 })
 
 app.get('/script.js', function(request, response) {
